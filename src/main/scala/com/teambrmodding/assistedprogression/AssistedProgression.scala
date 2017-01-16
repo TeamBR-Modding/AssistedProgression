0952ea6625717910f5bc2fce27924ff15a80db0f
Dyonovan <lookinforfun0000@gmail.com>
2017-01-16 17:25:58 -0500

    Initial commit

---
+++ src/main/scala/com/teambrmodding/assistedprogression/AssistedProgression.scala
@@ -0,0 +1,54 @@
+package com.teambrmodding.assistedprogression
+
+import com.teambr.bookshelf.common.CommonProxy
+import com.teambrmodding.assistedprogression.lib.Reference
+import net.minecraft.creativetab.CreativeTabs
+import net.minecraft.init.Items
+import net.minecraft.item.Item
+import net.minecraftforge.fml.common.Mod.EventHandler
+import net.minecraftforge.fml.common.event.{FMLInitializationEvent, FMLPostInitializationEvent, FMLPreInitializationEvent, FMLServerStartingEvent}
+import net.minecraftforge.fml.common.{Mod, SidedProxy}
+import org.apache.logging.log4j.LogManager
+
+/**
+  * This file was created for AssistedProgression
+  *
+  * AssistedProgression is licensed under the
+  * Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License:
+  * http://creativecommons.org/licenses/by-nc-sa/4.0/
+  *
+  * @author Dyonovan
+  * @since 1/16/2017
+  */
+@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION, dependencies = Reference.DEPENDENCIES, modLanguage = "scala")
+object AssistedProgression {
+
+    //The logger. For logging
+    final val logger = LogManager.getLogger(Reference.MOD_NAME)
+
+    var configFolderLocation : String = ""
+
+    @SidedProxy(clientSide = "com.teambrmodding.assistedprogression.client.ClientProxy",
+        serverSide = "com.teambrmodding.assistedprogression.common.CommonProxy")
+    var proxy : CommonProxy = null
+
+    val tabAssistedProgression: CreativeTabs = new CreativeTabs("tabAssistedProgression") {
+        override def getTabIconItem: Item = Items.BOOK
+    }
+
+    @EventHandler def preInit(event : FMLPreInitializationEvent) = {
+
+    }
+
+    @EventHandler def init(event : FMLInitializationEvent) =  {
+
+    }
+
+    @EventHandler def postInit(event : FMLPostInitializationEvent) = {
+
+    }
+
+    @EventHandler def serverLoad(event : FMLServerStartingEvent): Unit = {
+
+    }
+}
---
+++ src/main/scala/com/teambrmodding/assistedprogression/common/CommonProxy.scala
@@ -0,0 +1,15 @@
+package com.teambrmodding.assistedprogression.common
+
+/**
+  * This file was created for AssistedProgression
+  *
+  * AssistedProgression is licensed under the
+  * Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License:
+  * http://creativecommons.org/licenses/by-nc-sa/4.0/
+  *
+  * @author Dyonovan
+  * @since 1/16/2017
+  */
+class CommonProxy {
+
+}
