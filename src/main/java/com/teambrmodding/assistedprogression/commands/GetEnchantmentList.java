b0de151a9c9ae44b9c3c3d5f193af31f5689e539
Dyonovan <dyonovan@gmail.com>
2019-08-30 18:36:15 -0400

    Added javadoc before pauljoda yells at me

--- src/main/java/com/teambrmodding/assistedprogression/commands/GetEnchantmentList.java
+++ src/main/java/com/teambrmodding/assistedprogression/commands/GetEnchantmentList.java
@@ -1,10 +1,8 @@
 package com.teambrmodding.assistedprogression.commands;
 
 import com.mojang.brigadier.CommandDispatcher;
-import com.teambrmodding.assistedprogression.AssistedProgression;
 import com.teambrmodding.assistedprogression.lib.Reference;
 import net.minecraft.command.CommandSource;
-import net.minecraft.command.Commands;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -12,8 +10,24 @@ import org.apache.logging.log4j.LogManager;
 
 import static net.minecraft.command.Commands.literal;
 
+/**
+ * This file was created for AssistedProgression
+ *
+ * AssistedProgression is licensed under the
+ * Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License:
+ * http://creativecommons.org/licenses/by-nc-sa/4.0/
+ *
+ * @author James Rogers - Dyonovan
+ * @since 11/13/17
+ */
+
 public class GetEnchantmentList {
 
+    /**
+     * List all enchants that are registered. Outputs to console
+     *
+     * @param dispatcher {@link CommandDispatcher}
+     */
     public GetEnchantmentList(CommandDispatcher<CommandSource> dispatcher) {
         dispatcher.register(
                 literal("listEnchants")
@@ -27,16 +41,4 @@ public class GetEnchantmentList {
                 })
         );
     }
-
-    /*public GetEnchantmentList() {
-        dispatcher.register(
-                literal("listEnchants")
-                        .requires(cs -> cs.hasPermissionLevel(0))
-                        .executes(ctx -> {
-                            for (Enchantment enchantment : ForgeRegistries.ENCHANTMENTS.getValues()) {
-                                ResourceLocation enchant = enchantment.getRegistryName();
-                                AssistedProgression.LOGGER.warn(enchant.getNamespace() + "-" + enchant.getPath());
-                            });
-
-    }*/
 }
